97eecf4d641d6f54a16d41ed1b5c45d5e82e5306
ckponnappa <ckponnappa@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-04-15 19:18:09 +0000

    I like being committed
    git-svn-id: http://subversion.assembla.com/svn/shaker@7 843fd666-81f2-4710-8602-7759cdb5ad05

--- trunk/app/models/thought_worker.rb
+++
@@ -1,4 +0,0 @@
-class ThoughtWorker < ActiveRecord::Base
-  has_many  :guesses, :foreign_key => :guessing_thoughtworker_id
-  has_many  :received_guesses, :foreign_key => :receiving_thoughtworker_id, :class_name => 'Guess'
-end
