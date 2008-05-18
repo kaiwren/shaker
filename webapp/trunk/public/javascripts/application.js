196f75e5b89a06dd079212203df3d20db0e864fb
deobald <deobald@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-05-18 02:32:53 +0000

    pulled out javascript, added more jquery fun, added stars
    git-svn-id: http://subversion.assembla.com/svn/shaker@45 843fd666-81f2-4710-8602-7759cdb5ad05

--- webapp/trunk/public/javascripts/application.js
+++ webapp/trunk/public/javascripts/application.js
@@ -1,3 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function initJQueryExtensions() {
+  jQuery.extend(jQuery.expr[':'], {
+    containsIgnoreCase: "(a.textContent||a.innerText||jQuery(a).text()||'').toLowerCase().indexOf((m[3]||'').toLowerCase())>=0"
+  });
+}
+
+function adjustCss() {
+  $(".instruction > img").addClass('inline-image');
+  $(".guess-count > img").addClass('inline-image');
+}
+
+function doFilter(value){
+  $("tr.row").removeClass('visible').addClass('hidden');
+  $("tr.row:containsIgnoreCase('" + value + "')").removeClass('hidden').addClass('visible');
+}
+
