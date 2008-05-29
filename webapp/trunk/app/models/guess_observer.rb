5f37e12a9874eed7182c244b579326312a14a56d
ckponnappa <ckponnappa@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-05-29 20:59:59 +0000

    added watcher email
    git-svn-id: http://subversion.assembla.com/svn/shaker@62 843fd666-81f2-4710-8602-7759cdb5ad05

--- webapp/trunk/app/models/guess_observer.rb
+++ webapp/trunk/app/models/guess_observer.rb
@@ -2,7 +2,7 @@ class GuessObserver < ActiveRecord::Observer
   def after_save(guess)
     receiving_user = guess.receiving_user
     if receiving_user.notify_listeners?
-      receiving_user.listeners{|listener|
+      receiving_user.listeners.each{|listener|
         UserNotifier.deliver_watched_user_salary_visible(listener, receiving_user)
       }
     end
