9ee523b1988a17d11cdfea8e5139b22b3ae904a7
deobald <deobald@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-05-29 20:50:53 +0000

    jQuery done right
    git-svn-id: http://subversion.assembla.com/svn/shaker@60 843fd666-81f2-4710-8602-7759cdb5ad05

--- webapp/trunk/public/javascripts/application.js
+++ webapp/trunk/public/javascripts/application.js
@@ -1,20 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function initJQueryExtensions() {
+function initJQuery() {
     jQuery.extend(jQuery.expr[':'], {
-        containsIgnoreCase: "(a.textContent||a.innerText||jQuery(a).text()||'').toLowerCase().indexOf((m[3]||'').toLowerCase())>=0"
+      containsIgnoreCase: "(a.textContent||a.innerText||jQuery(a).text()||'').toLowerCase().indexOf((m[3]||'').toLowerCase())>=0"
     });
+
+    $("#filter-users").bind('keyup', function() {
+        $("tr.row").removeClass('visible').addClass('hidden');
+        $("tr.row:containsIgnoreCase('" + this.value + "')").removeClass('hidden').addClass('visible');
+    });
+
 }
 
 function adjustCss() {
     $(".instruction > img").addClass('inline-image');
     $(".guess-count > img").addClass('inline-image');
-}
-
-function doFilter(value) {
-    $("tr.row").removeClass('visible').addClass('hidden');
-    $("tr.row:containsIgnoreCase('" + value + "')").removeClass('hidden').addClass('visible');
 }
 
 function watch_user(target_id, listening_id) {
